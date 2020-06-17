f481d06b6752dcbca9c542ced044bfd03b9cdd0a
Alexander Shvyrev <shurik_stv@yahoo.com>
2020-06-17 13:24:04 -0400

    initial commit

---
+++ src/build/precache-manifest.e0baecac97ebb55d9a38c650b60e5f08.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9fb67dd4b89de1dcfdafa571b5bbe310",
+    "url": "/index.html"
+  },
+  {
+    "revision": "486df8ea5bfd8dfb7e94",
+    "url": "/static/css/2.a5f7a400.chunk.css"
+  },
+  {
+    "revision": "75ee0ec28a2cda83b9d2",
+    "url": "/static/css/main.4e71ce81.chunk.css"
+  },
+  {
+    "revision": "486df8ea5bfd8dfb7e94",
+    "url": "/static/js/2.9d9734b6.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.9d9734b6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "75ee0ec28a2cda83b9d2",
+    "url": "/static/js/main.a7dc0528.chunk.js"
+  },
+  {
+    "revision": "8931283fa7532b66ee7f",
+    "url": "/static/js/runtime-main.fca178c7.js"
+  }
+]);
